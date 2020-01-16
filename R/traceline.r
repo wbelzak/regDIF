c8b50916a62d849591f2c0690d623b805604a54e
Will Belzak <wbelzak@live.unc.edu>
2020-01-16 15:07:19 -0500

    .

--- R/traceline.r
+++ R/traceline.r
@@ -8,5 +8,4 @@ trace.line.pts <-
     active_cov_a <- as.matrix(covariates[,as.numeric(gsub(".*([0-9]+)", "\\1", names(p_active[grep("a1",names(p_active))])))])
 
     traceline <- 1/(1+exp(-((p_active[grep("c0",names(p_active))] + active_cov_c %*% p_active[grep("c1",names(p_active))]) + (p_active[grep("a0",names(p_active))] + active_cov_a %*% p_active[grep("a1",names(p_active))])*theta))) #logistic CDF
-
   }

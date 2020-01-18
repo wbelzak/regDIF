aac0161d9756af699c5fb378de3f5ba52ee01ff9
Will Belzak <wbelzak@live.unc.edu>
2020-01-18 11:30:57 -0500

    efficient traceline

--- R/traceline.r
+++ R/traceline.r
@@ -4,8 +4,7 @@
 
 trace.line.pts <-
   function(p_active,theta,covariates) {
-    active_cov_c <- as.matrix(covariates[,as.numeric(gsub(".*([0-9]+)", "\\1", names(p_active[grep("c1",names(p_active))])))])
-    active_cov_a <- as.matrix(covariates[,as.numeric(gsub(".*([0-9]+)", "\\1", names(p_active[grep("a1",names(p_active))])))])
-
-    traceline <- 1/(1+exp(-((p_active[grep("c0",names(p_active))] + active_cov_c %*% p_active[grep("c1",names(p_active))]) + (p_active[grep("a0",names(p_active))] + active_cov_a %*% p_active[grep("a1",names(p_active))])*theta))) #logistic CDF
+    traceline <- sapply(theta,
+                       function(x){
+                         1/(1+exp(-((p_active[grep("c0",names(p_active))] + covariates %*% p_active[grep("c1",names(p_active))]) + (p_active[grep("a0",names(p_active))] + covariates %*% p_active[grep("a1",names(p_active))])*x)))})
   }

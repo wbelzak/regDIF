19427aa4c56dd6135572c2d29897c8e8d2c2eb84
Will Belzak <wbelzak@live.unc.edu>
2020-01-18 11:29:33 -0500

    efficient traceline computing

--- R/derivatives.r
+++ R/derivatives.r
@@ -2,12 +2,11 @@
 # Partial Derivatives #
 #######################
 
-
 d <-
   function(parm,p,r1,r0,data,item.focus,theta,covariates,cov,num_items,samp_size,num_quadpts) {
 
     #make space for the trace lines and the derivative E-tables
-    logp_deriv <- logq_deriv <- logpq_deriv2 <- matrix(0, nrow = samp_size, ncol = num_quadpts)
+    traceline <- matrix(0, nrow = samp_size, ncol = num_quadpts)
     eta.d <- matrix(0, nrow = samp_size, ncol = num_quadpts)
 
     if(parm == "c0"){
@@ -21,18 +20,12 @@ d <-
     }
 
     p_active <- c(p[paste0("c0_itm",item.focus,"_")],p[grep(paste0("c1_itm",item.focus,"_"),names(p))],p[paste0("a0_itm",item.focus,"_")],p[grep(paste0("a1_itm",item.focus,"_"),names(p))])
-    for(q in 1:num_quadpts){
-      logp_deriv[,q] <- (1-trace.line.pts(p_active,theta[q],covariates))*eta.d[,q]
-      logq_deriv[,q] <- -trace.line.pts(p_active,theta[q],covariates)*eta.d[,q]
-      logpq_deriv2[,q] <- -eta.d[,q]**2*trace.line.pts(p_active,theta[q],covariates)*(1-trace.line.pts(p_active,theta[q],covariates))
-    }
+    traceline <- trace.line.pts(p_active,theta,covariates)
 
-    d1 <- sum(logp_deriv*r1) + sum(logq_deriv*r0)
-    d2 <- sum(logpq_deriv2*r1) + sum(logpq_deriv2*r0)
+    d1 <- sum((1-traceline)*eta.d*r1) + sum(-traceline*eta.d*r0)
+    d2 <- sum(-traceline*(1-traceline)*eta.d**2*r1) + sum(-traceline*(1-traceline)*eta.d**2*r0)
     dlist <- list(d1,d2)
   }
-
-
 
 
 

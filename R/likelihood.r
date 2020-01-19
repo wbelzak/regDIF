7933e3c348db75076e50e4c2fd6b70985fbf92cc
Will Belzak <wbelzak@live.unc.edu>
2020-01-19 18:14:38 -0500

    more efficient grep

--- R/likelihood.r
+++ R/likelihood.r
@@ -4,10 +4,10 @@
 
 ll.2pl.impact <-
   function(p_impact,nr,theta,covariates,samp_size,num_quadpts) {
-    alpha <- covariates %*% p_impact[grep("g0",names(p_impact))]
-    phi <- exp(covariates %*% p_impact[grep("b0",names(p_impact))])
+    alpha <- covariates %*% p_impact[grep("g0",names(p_impact),fixed=T)]
+    phi <- exp(covariates %*% p_impact[grep("b0",names(p_impact),fixed=T)])
 
-    theta_scores2 <- t(sapply(1:samp_size, function(x){dnorm(theta, mean = alpha[x], sd = sqrt(phi[x]))}))
+    theta_scores <- t(sapply(1:samp_size, function(x){dnorm(theta, mean = alpha[x], sd = sqrt(phi[x]))}))
 
     ll_impact <- (-1)*(sum(nr*log(theta_scores)))
   }

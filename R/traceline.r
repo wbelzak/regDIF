8ddc05c41f562fcb1407dae7adb569604888d3f9
Will Belzak <wbelzak@live.unc.edu>
2020-01-19 18:15:29 -0500

    more efficient grep

--- R/traceline.r
+++ R/traceline.r
@@ -6,5 +6,5 @@ trace.line.pts <-
   function(p_active,theta,covariates) {
     traceline <- sapply(theta,
                        function(x){
-                         1/(1+exp(-((p_active[grep("c0",names(p_active))] + covariates %*% p_active[grep("c1",names(p_active))]) + (p_active[grep("a0",names(p_active))] + covariates %*% p_active[grep("a1",names(p_active))])*x)))})
+                         1/(1+exp(-((p_active[grep("c0",names(p_active),fixed=T)] + covariates %*% p_active[grep("c1",names(p_active),fixed=T)]) + (p_active[grep("a0",names(p_active),fixed=T)] + covariates %*% p_active[grep("a1",names(p_active),fixed=T)])*x)))})
   }

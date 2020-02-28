ca0096edfcb995ea5f5bb13f89303f9e55e1c558
Will Belzak <wbelzak@live.unc.edu>
2020-02-28 12:38:34 -0500

    update

--- tests/testthat/test-regDIF.R
+++ tests/testthat/test-regDIF.R
@@ -1,5 +1,5 @@
 context("test-regDIF")
 
 test_that("Warning for not dichotomous responses works", {
-  expect_error(regDIF(mtcars[,1:4], mtcars[,6], 0), "Data must be scored 1 for yes/correct and 0 for no/incorrect.")
+  expect_error(regDIF(mtcars[,1:4], mtcars[,6], 0), "Item responses must be scored 1 for yes/correct and 0 for no/incorrect.")
 })

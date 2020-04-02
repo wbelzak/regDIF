6801d78b1c6d404ace5f7639d39575288dffd4fc
Will Belzak <wbelzak@live.unc.edu>
2020-04-02 12:43:06 -0400

    organize

--- R/threshold.R
+++ R/threshold.R
@@ -1,13 +1,29 @@
-soft_threshold <- function(z,lambda){
+################
+# Thresholding #
+################
+
+#lasso penalty
+soft_threshold <-
+  function(z,
+           lambda) {
+
   p_new <- sign(z)*max(abs(z) - lambda, 0)
+
   return(p_new)
+
 }
 
-firm_threshold <- function(z,lambda,gamma){
+#mcp penalty
+firm_threshold <-
+  function(z,
+           lambda,
+           gamma) {
+
   if(abs(z) <= gamma*lambda){
     p_new <- (gamma/(gamma-1))*soft_threshold(z,lambda)
   }else{
     p_new <- z
   }
+
   return(p_new)
 }

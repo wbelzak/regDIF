6196ff5abcb6c54b7137afab7beea14368c3b51a
Will Belzak <wbelzak@live.unc.edu>
2020-04-06 08:15:05 -0400

    add regdif package documentation

---
+++ R/regDIF-package.R
@@ -0,0 +1,13 @@
+#' Regularized Differential Item Functioning
+#'
+#' \pkg{regDIF} is a package that performs regularization of differential item functioning (DIF)
+#' in item response theory (IRT) and confirmatory factor analysis (CFA) models using a
+#' penalized expectation-maximization algorithm.
+#'
+#' @name regDIF-package
+#' @docType package
+#' @title Regularized differential item functioning for IRT and CFA models.
+#' @author William Belzak \email{wbelzak@@gmail.com}
+#' @exportMethod coef plot print
+#' @keywords package
+NULL

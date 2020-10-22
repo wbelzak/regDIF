56aa31f73054fbc893c738a1339f042a18700369
William Belzak <wbelzak@gmail.com>
2020-10-22 13:02:58 -0400

    fix tests and vignette for changes in function arguments

--- tests/testthat/test-regDIF.R
+++ tests/testthat/test-regDIF.R
@@ -3,7 +3,6 @@ context("test-regDIF")
 test_that("Stop for negative tuning values.", {
   expect_error(regDIF(ida[,1:6],
                       ida[,7:9],
-                      item.type = "binary",
-                      penalty = "lasso", tau = -1),
+                      tau = -1),
                "Tau values must be non-negative.")
 })

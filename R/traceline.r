0bb2618518d8540b6916c23e2fae8b69e3d9efe7
William Belzak <wbelzak@gmail.com>
2021-01-10 11:10:34 -0500

    delete categorical traceline (using cumulative tracelines instead), and revert to using vector of theta values

--- R/traceline.r
+++ R/traceline.r
@@ -1,11 +1,9 @@
 #' Binary item tracelines.
 #'
 #' @param p_active Vector of item parameters.
-#' @param theta Matrix of adaptive theta values.
+#' @param theta Vector of theta values.
 #' @param pred.data Matrix or dataframe of DIF and/or impact predictors.
 #' @param samp_size Sample size in dataset.
-#' @param quad.pts Number of quadrature points used for approximating the
-#' latent variable.
 #'
 #' @keywords internal
 #'
@@ -13,158 +11,34 @@ bernoulli_traceline_pts <-
   function(p_active,
            theta,
            pred.data,
-           samp_size,
-           quad.pts) {
+           samp_size) {
 
     c0_parms <- grepl("c0",names(p_active),fixed=T)
     c1_parms <- grepl("c1",names(p_active),fixed=T)
     a0_parms <- grepl("a0",names(p_active),fixed=T)
     a1_parms <- grepl("a1",names(p_active),fixed=T)
 
-    traceline0 <-
-      apply(theta,
-            2,
-            function(x) {
-              1 - 1 / (1 + exp(-((p_active[c0_parms] +
+    traceline <- vapply(theta,
+                        function(x) {1 / (1 + exp(-((p_active[c0_parms] +
                                   pred.data %*% p_active[c1_parms]) +
                                    (p_active[a0_parms] +
                                       pred.data %*% p_active[a1_parms])*x)
                                )
                        )
-              })
-    traceline1 <- -traceline0 + 1
+              },numeric(samp_size))
 
-    return(list(traceline0,traceline1))
-
-  }
-
-#' Ordinal item tracelines (graded response).
-#'
-#' @param p_active Vector of item parameters.
-#' @param theta Matrix of adaptive theta values.
-#' @param pred.data Matrix or dataframe of DIF and/or impact predictors.
-#' @param samp_size Sample size in dataset.
-#' @param num_responses_item Number of responses for item.
-#' @param quad.pts Number of quadrature points used for approximating the
-#' latent variable.
-#'
-#' @keywords internal
-#'
-categorical_traceline_pts <-
-  function(p_active,
-           theta,
-           pred.data,
-           samp_size,
-           num_responses_item,
-           quad.pts) {
-
-  # Space for category traceline (y = c category).
-  traceline <- replicate(n=num_responses_item,
-                         matrix(0,nrow=samp_size,ncol=quad.pts),
-                         simplify = F)
-
-  c0_parms <- grepl("c0",names(p_active),fixed=T)
-  c1_parms <- grepl("c1",names(p_active),fixed=T)
-  a0_parms <- grepl("a0",names(p_active),fixed=T)
-  a1_parms <- grepl("a1",names(p_active),fixed=T)
-
-  # For item response 1.
-  traceline[[1]] <-
-    apply(theta,
-          2,
-          function(x) {
-            1 - 1 / (1 + exp(-((p_active[c0_parms][1] +
-                                  pred.data %*% p_active[c1_parms]) +
-                                 (p_active[a0_parms] +
-                                    pred.data %*% p_active[a1_parms])*x)
-                             )
-                     )
-            })
-
-  #for item response 2
-  if(num_responses_item > 2) {
-  traceline[[2]] <-
-    apply(theta,
-          2,
-          function(x) {
-            1 / (1 + exp(-((p_active[c0_parms][1] +
-                              pred.data %*% p_active[c1_parms]) +
-                             (p_active[a0_parms] +
-                                pred.data %*% p_active[a1_parms])*x)
-                         )
-                 )
-            }) -
-    apply(theta,
-          2,
-          function(x) {
-            1 / (1 + exp(-((p_active[c0_parms][1] -
-                              p_active[c0_parms][2] +
-                              pred.data %*% p_active[c1_parms]) +
-                             (p_active[a0_parms] +
-                                pred.data %*% p_active[a1_parms])*x)
-                         )
-                 )
-            })
-
-    # For item responses 3 to J-1.
-    if(num_responses_item > 3) {
-      for(thr in 3:(num_responses_item-1)) {
-        traceline[[thr]] <-
-          apply(theta,
-                2,
-                function(x) {
-                  1 / (1 + exp(-((p_active[c0_parms][1] -
-                                    p_active[c0_parms][thr-1] +
-                                    pred.data %*% p_active[c1_parms]) +
-                                   (p_active[a0_parms] +
-                                      pred.data %*% p_active[a1_parms])*x)
-                               )
-                       )
-                  }) -
-          apply(theta,
-                2,
-                function(x) {
-                  1 / (1 + exp(-((p_active[c0_parms][1] -
-                                    p_active[c0_parms][thr] +
-                                    pred.data %*% p_active[c1_parms]) +
-                                   (p_active[a0_parms] +
-                                      pred.data %*% p_active[a1_parms])*x)
-                               )
-                       )
-                  })
-
-      }
-
-    }
-
-  }
-
-  # For item response J.
-  traceline[[num_responses_item]] <-
-    apply(theta,
-          2,
-          function(x) {
-            1 / (1 + exp(-((p_active[c0_parms][1] -
-                              p_active[c0_parms][num_responses_item-1] +
-                              pred.data %*% p_active[c1_parms]) +
-                             (p_active[a0_parms] +
-                                pred.data %*% p_active[a1_parms])*x)
-                         )
-                 )
-            })
-
-  return(traceline)
+    return(traceline)
 
   }
 
 #' Ordinal tracelines (for derivatives).
 #'
 #' @param p_active Vector of item parameters.
-#' @param theta Matrix of adaptive theta values.
+#' @param theta Vector of theta values.
 #' @param pred.data Matrix or dataframe of DIF and/or impact predictors.
 #' @param samp_size Sample size in dataset.
 #' @param num_responses_item Number of responses for item.
-#' @param quad.pts Number of quadrature points used for approximating the
+#' @param num.quad Number of quadrature points used for approximating the
 #' latent variable.
 #'
 #' @keywords internal
@@ -175,12 +49,12 @@ cumulative_traceline_pts <-
            pred.data,
            samp_size,
            num_responses_item,
-           quad.pts) {
+           num.quad) {
 
   # Space for cumulative traceline (y >= c category).
   traceline <-
     replicate(n=(num_responses_item-1),
-              matrix(0,nrow=samp_size,ncol=quad.pts),
+              matrix(0,nrow=samp_size,ncol=num.quad),
               simplify = F)
 
   c0_parms <- grepl("c0",names(p_active),fixed=T)
@@ -190,8 +64,7 @@ cumulative_traceline_pts <-
 
   # For item response 1.
   traceline[[1]] <-
-    apply(theta,
-          2,
+    vapply(theta,
           function(x) {
             1 / (1 + exp(-((p_active[c0_parms][1] +
                               pred.data %*% p_active[c1_parms]) +
@@ -199,14 +72,12 @@ cumulative_traceline_pts <-
                                 pred.data %*% p_active[a1_parms])*x)
                          )
                  )
-            })
+            }, numeric(samp_size))
 
   # For item response 2 to J.
-  if(num_responses_item > 2) {
     for(thr in 2:(num_responses_item-1)) {
       traceline[[thr]] <-
-        apply(theta,
-              2,
+        vapply(theta,
               function(x) {
                 1 / (1 + exp(-((p_active[c0_parms][1] -
                                   p_active[c0_parms][thr] +
@@ -215,9 +86,8 @@ cumulative_traceline_pts <-
                                     pred.data %*% p_active[a1_parms])*x)
                              )
                      )
-                })
+                }, numeric(samp_size))
     }
-  }
 
   return(traceline)
 
@@ -226,12 +96,10 @@ cumulative_traceline_pts <-
 #' Continuous tracelines.
 #'
 #' @param p_active Vector of item parameters.
-#' @param theta Matrix of adaptive theta values.
+#' @param theta Vector of theta values.
 #' @param responses_item Vector of item responses.
 #' @param pred.data Matrix or dataframe of DIF and/or impact predictors.
 #' @param samp_size Sample size in dataset.
-#' @param quad.pts Number of quadrature points used for approximating the
-#' latent variable.
 #'
 #' @keywords internal
 #'
@@ -240,8 +108,7 @@ gaussian_traceline_pts <-
            theta,
            responses_item,
            pred.data,
-           samp_size,
-           quad.pts) {
+           samp_size) {
 
   c0_parms <- grepl("c0",names(p_active),fixed=T)
   c1_parms <- grepl("c1",names(p_active),fixed=T)
@@ -251,14 +118,13 @@ gaussian_traceline_pts <-
   s1_parms <- grepl("s1",names(p_active),fixed=T)
 
   mu <-
-    apply(theta,
-          2,
+    vapply(theta,
           function(x) {
             (p_active[c0_parms] +
                pred.data %*% p_active[c1_parms]) +
               (p_active[a0_parms] +
                  pred.data %*% p_active[a1_parms])*x
-            })
+            }, numeric(samp_size))
   sigma <-
     sqrt(p_active[s0_parms][1]*exp(pred.data %*% p_active[s1_parms]))
 

7086fcbb5ad4b77e72b6d3139d6f43576fb33c31
William Belzak <wbelzak@gmail.com>
2021-07-27 17:27:19 -0400

    add proxy derivatives for multivariate regression

--- R/derivatives.r
+++ R/derivatives.r
@@ -49,6 +49,47 @@ d_alpha <-
 
 }
 
+#' Partial derivatives for mean impact equation using proxy data.
+#'
+#' @param p_impact Vector of impact parameters.
+#' @param prox_data Matrix of observed proxy scores.
+#' @param mean_predictors Possibly different matrix of predictors for the mean
+#' impact equation.
+#' @param var_predictors Possibly different matrix of predictors for the
+#' variance impact equation.
+#' @param cov Covariate being maximized.
+#' @param samp_size Sample size in data set.
+#' @param num_items Number of items in data set.
+#'
+#' @return a \code{"list"} of first and second partial derivatives for mean impact equation (to
+#' use with coordinate descent and univariate Newton-Raphson)
+#'
+#' @keywords internal
+#'
+d_alpha_proxy <-
+  function(p_impact,
+           prox_data,
+           mean_predictors,
+           var_predictors,
+           cov,
+           samp_size,
+           num_items) {
+
+    # Get latent mean and variance vectors.
+    alpha <- mean_predictors %*% p_impact[grep("mean",names(p_impact),fixed=T)]
+    phi <- exp(var_predictors %*% p_impact[grep("var",names(p_impact),fixed=T)])
+
+    d1_trace <- mean_predictors[,cov]/phi*(prox_data-alpha)
+    d2_trace <- -mean_predictors[,cov]**2/phi
+
+    d1 <- sum(d1_trace, na.rm = TRUE)
+    d2 <- sum(d2_trace, na.rm = TRUE)
+
+    dlist <- list(d1,d2)
+
+  }
+
+
 #' Partial derivatives for mean impact equation.
 #'
 #' @param p_impact Vector of impact parameters.
@@ -101,6 +142,50 @@ d_phi <-
   d2 <- sum(etable*d2_trace, na.rm = TRUE)
 
   dlist <- list(d1,d2)
+
+  }
+
+#' Partial derivatives for mean impact equation using proxy data.
+#'
+#' @param p_impact Vector of impact parameters.
+#' @param prox_data Matrix of observed proxy scores.
+#' @param mean_predictors Possibly different matrix of predictors for the mean
+#' impact equation.
+#' @param var_predictors Possibly different matrix of predictors for the
+#' variance impact equation.
+#' @param cov Covariate being maximized.
+#' @param samp_size Sample size in dataset.
+#' @param num_items Number of items in dataset.
+#'
+#' @return a \code{"list"} of first and second partial derivatives for variance impact equation (to
+#' use with coordinate descent and univariate Newton-Raphson)
+#'
+#' @keywords internal
+#'
+d_phi_proxy <-
+  function(p_impact,
+           prox_data,
+           mean_predictors,
+           var_predictors,
+           cov,
+           samp_size,
+           num_items) {
+
+    # Get latent mean and variance vectors
+    alpha <- mean_predictors %*% p_impact[grep("mean",names(p_impact),fixed=T)]
+    phi <- exp(var_predictors %*% p_impact[grep("var",names(p_impact),fixed=T)])
+
+    eta_d1 <- .5*sqrt(phi)*var_predictors[,cov]
+    eta_d2 <- .5*sqrt(phi)*var_predictors[,cov]**2
+
+    d1_trace <- eta_d1*((prox_data-alpha)**2/phi**(3/2) -
+                                   1/sqrt(phi))
+    d2_trace <- -eta_d2*(phi**(-3/2)*(prox_data-alpha)**2)
+
+    d1 <- sum(d1_trace, na.rm = TRUE)
+    d2 <- sum(d2_trace, na.rm = TRUE)
+
+    dlist <- list(d1,d2)
 
   }
 
@@ -379,6 +464,64 @@ d_bernoulli <-
             na.rm = TRUE)
 
   dlist <- list(d1,d2)
+
+  }
+
+#' Partial derivatives for binary items with proxy data.
+#'
+#' @param parm Item parameter being maximized.
+#' @param p_item Vector of item parameters.
+#' @param prox_data Vector of observed proxy scores.
+#' @param pred_data Matrix or dataframe of DIF and/or impact predictors.
+#' @param item_data_current Vector of current item responses.
+#' @param cov Covariate being maximized.
+#' @param samp_size Sample size in dataset.
+#' @param num_items Number of items in dataset.
+#'
+#' @return a \code{"list"} of first and second partial derivatives for Bernoulli item likelihood (to
+#' use with coordinate descent and univariate Newton-Raphson)
+#'
+#' @keywords internal
+#'
+d_bernoulli_proxy <-
+  function(parm,
+           p_item,
+           prox_data,
+           pred_data,
+           item_data_current,
+           cov,
+           samp_size,
+           num_items) {
+
+    if(parm == "c0"){
+      eta_d <- matrix(1, nrow = samp_size, ncol = 1)
+    } else if(parm == "a0"){
+      eta_d <- prox_data
+    } else if(parm == "c1"){
+      eta_d <- matrix(pred_data[,cov],
+                      ncol = 1,
+                      nrow = samp_size)
+    } else if(parm == "a1"){
+      eta_d <- matrix(pred_data[,cov],
+                      ncol = 1,
+                      nrow = samp_size)*prox_data
+    }
+
+    traceline <- bernoulli_traceline_pts_proxy(p_item,
+                                               prox_data,
+                                               pred_data)
+
+    d1_base <- matrix(0, nrow = nrow(traceline), ncol = 1)
+    d1_base[item_data_current == 1,] <- -traceline[item_data_current == 1,]
+    d1_base[item_data_current == 2,] <- (1 - traceline)[item_data_current == 2,]
+
+    d1 <- sum(eta_d*d1_base,
+              na.rm = TRUE)
+
+    d2 <- sum(eta_d**2*(-traceline + traceline**2),
+              na.rm = TRUE)
+
+    dlist <- list(d1,d2)
 
   }
 
@@ -685,6 +828,7 @@ d_bernoulli_itemblock_proxy <-
 #' @param thr Threshold value being maximized.
 #' @param cov Covariate being maximized.
 #' @param samp_size Sample size in dataset.
+#' @param num_responses_item Number of responses for item.
 #' @param num_items Number of items in dataset.
 #' @param num_quad Number of quadrature points used for approximating the
 #' latent variable.
@@ -793,6 +937,123 @@ d_categorical <-
 
   }
 
+#' Partial derivatives for ordinal items using proxy data.
+#'
+#' @param parm Item parameter being maximized.
+#' @param p_item Vector of item parameters.
+#' @param prox_data Vector of observed proxy scores.
+#' @param pred_data Matrix or dataframe of DIF and/or impact predictors.
+#' @param item_data_current Vector of current item responses.
+#' @param thr Threshold value being maximized.
+#' @param cov Covariate being maximized.
+#' @param samp_size Sample size in dataset.
+#' @param num_responses_item Number of responses for item.
+#' @param num_items Number of items in dataset.
+#'
+#' @return a \code{"list"} of first and second partial derivatives for categorical item likelihood
+#' (to use with coordinate descent and univariate Newton-Raphson)
+#'
+#' @keywords internal
+#'
+d_categorical_proxy <-
+  function(parm,
+           p_item,
+           prox_data,
+           pred_data,
+           item_data_current,
+           thr,
+           cov,
+           samp_size,
+           num_responses_item,
+           num_items) {
+
+    if(parm == "c0"){
+      eta_d <- matrix(1, nrow = samp_size, ncol = 1)
+    } else if(parm == "a0"){
+      eta_d <- prox_data
+    } else if(parm == "c1"){
+      eta_d <- matrix(pred_data[,cov],
+                      ncol = 1,
+                      nrow = samp_size)
+    } else if(parm == "a1"){
+      eta_d <- matrix(pred_data[,cov],
+                      ncol = 1,
+                      nrow = samp_size)*prox_data
+    }
+
+    cum_traceline <- cumulative_traceline_pts_proxy(p_item,
+                                                    prox_data,
+                                                    pred_data,
+                                                    samp_size,
+                                                    num_responses_item)
+
+
+    d1_base <- lapply(1:num_responses_item, function(x) matrix(1, nrow = samp_size, ncol = 1))
+    for(resp in 1:num_responses_item) {
+      d1_base[[resp]][!(item_data_current == resp)] <- 0
+    }
+
+    # Non-threshold derivatives.
+    if(thr < 0){
+      d1 <- eta_d*(-d1_base[[1]]*cum_traceline[[1]] +
+                     d1_base[[num_responses_item]]*(
+                       1 - cum_traceline[[num_responses_item-1]]))
+      d2 <- eta_d**2*(-d1_base[[1]]*(cum_traceline[[1]]*(
+        1-cum_traceline[[1]])) +
+          d1_base[[num_responses_item]]*(
+            -cum_traceline[[num_responses_item-1]]*(
+              1-cum_traceline[[num_responses_item-1]])))
+
+      for(i in 2:(num_responses_item-1)){
+
+        # Skip intermediate derivative calculations for constrained theshold.
+        d1 <- d1 + eta_d*d1_base[[i]]*((1-cum_traceline[[i]]) -
+                                             cum_traceline[[i-1]])
+        d2 <- d2 + eta_d**2*d1_base[[i]]*(cum_traceline[[i-1]]**2 +
+                                                cum_traceline[[i]]**2 -
+                                                cum_traceline[[i-1]] -
+                                                cum_traceline[[i]])
+      }
+
+
+      d1 <- sum(d1, na.rm = TRUE)
+      d2 <- sum(d2, na.rm = TRUE)
+
+      # Threshold derivatives.
+    } else {
+      if(thr < (num_responses_item-1)) {
+        cat_traceline <- (cum_traceline[[thr]] - cum_traceline[[thr+1]])
+      } else{
+        cat_traceline <- cum_traceline[[thr]]
+      }
+      d1 <-
+        sum(-d1_base[[thr]]*cum_traceline[[thr]]*(
+          1 - cum_traceline[[thr]]
+        ) / (cum_traceline[[thr-1]] - cum_traceline[[thr]]), na.rm = TRUE) +
+        sum(d1_base[[thr+1]]*cum_traceline[[thr]]*(
+          1 - cum_traceline[[thr]]
+        ) / cat_traceline, na.rm = TRUE)
+      d2 <- sum(d1_base[[thr]] /
+                  (cum_traceline[[thr-1]] -
+                     cum_traceline[[thr]])*(
+                       cum_traceline[[thr]]*(1 - cum_traceline[[thr]])**2 -
+                         cum_traceline[[thr]]**2*(1 - cum_traceline[[thr]]) +
+                         cum_traceline[[thr]]**2*(1 - cum_traceline[[thr]])**2 /
+                         (cum_traceline[[thr-1]] - cum_traceline[[thr]])
+                     ), na.rm = TRUE) -
+        sum(d1_base[[thr+1]] / cat_traceline*(
+          cum_traceline[[thr]]*(1 - cum_traceline[[thr]])**2 -
+            cum_traceline[[thr]]**2*(1-cum_traceline[[thr]]) -
+            cum_traceline[[thr]]**2*(1-cum_traceline[[thr]])**2 /
+            cat_traceline
+        ), na.rm = TRUE)
+
+    }
+
+    dlist <- list(d1,d2)
+
+  }
+
 #' Partial derivatives for ordinal items.
 #'
 #' @param parm Item parameter being maximized.
@@ -802,6 +1063,7 @@ d_categorical <-
 #' @param pred_data Matrix or dataframe of DIF and/or impact predictors.
 #' @param item_data_current Vector of current item responses.
 #' @param samp_size Sample size in dataset.
+#' @param num_repsones_item Number of responses for item.
 #' @param num_items Number of items in dataset.
 #' @param num_predictors Number of predictors in dataset.
 #' @param num_quad Number of quadrature points used for approximating the
@@ -1098,7 +1360,70 @@ d_mu_gaussian <-
 
   dlist <- list(d1,d2)
 
-}
+  }
+
+#' Partial derivatives for mean parameter of continuous items with proxy data.
+#'
+#' @param parm Item parameter being maximized.
+#' @param p_item Vector of item parameters.
+#' @param prox_data Vector of observed proxy scores.
+#' @param responses_item Vector of item responses.
+#' @param pred_data Matrix or dataframe of DIF and/or impact predictors.
+#' @param cov Covariate being maximized.
+#' @param samp_size Sample size in dataset.
+#'
+#' @return a \code{"list"} of first and second partial derivatives for mean value of Gaussian item
+#' likelihood (to use with coordinate descent and univariate Newton-Raphson)
+#'
+#' @keywords internal
+#'
+d_mu_gaussian_proxy <-
+  function(parm,
+           p_item,
+           prox_data,
+           responses_item,
+           pred_data,
+           cov,
+           samp_size) {
+
+    if(parm == "c0"){
+      eta_d <- matrix(1, nrow = samp_size, ncol = 1)
+    } else if(parm == "a0"){
+      eta_d <- prox_data
+    } else if(parm == "c1"){
+      eta_d <- as.matrix(pred_data[,cov])
+    } else if(parm == "a1"){
+      eta_d <- as.matrix(pred_data[,cov]*prox_data)
+    }
+
+
+    # Get latent mean and variance vectors.
+    mu <- (p_item[grep("c0",names(p_item),fixed=T)] +
+                      pred_data %*%
+                      p_item[grep("c1",names(p_item),fixed=T)]) +
+                     (p_item[grep("a0",names(p_item),fixed=T)] +
+                        pred_data %*%
+                        p_item[grep("a1",names(p_item),fixed=T)])*prox_data
+    sigma <- sqrt(p_item[grep("s0",names(p_item))][1]*exp(
+      pred_data %*% p_item[grep("s1",names(p_item))]
+    ))
+
+
+    d1_trace <- t(sapply(1:samp_size,
+                         function(x) {
+                           eta_d[x,]/sigma[x]**2*(responses_item[x] - mu[x,])
+                         }))
+    d2_trace <- t(sapply(1:samp_size,
+                         function(x) {
+                           -eta_d[x,]**2 / sigma[x]**2
+                         }))
+
+    d1 <- sum(d1_trace, na.rm = TRUE)
+    d2 <- sum(d2_trace, na.rm = TRUE)
+
+    dlist <- list(d1,d2)
+
+  }
 
 #' Partial derivatives for variance parameter of continuous items.
 #'
@@ -1198,5 +1523,93 @@ d_sigma_gaussian <-
 
 }
 
+#' Partial derivatives for variance parameter of continuous items with proxy data.
+#'
+#' @param parm Item parameter being maximized.
+#' @param p_item Vector of item parameters.
+#' @param prox_data Vector of observed proxy scores.
+#' @param responses_item Vector of item responses.
+#' @param pred_data Matrix or dataframe of DIF and/or impact predictors.
+#' @param cov Covariate being maximized.
+#' @param samp_size Sample size in dataset.
+#' @param num_items Number of items in dataset.
+#'
+#' @return a \code{"list"} of first and second partial derivatives for variance value of Gaussian
+#' item likelihood (to use with coordinate descent and univariate Newton-Raphson)
+#'
+#' @keywords internal
+#'
+d_sigma_gaussian_proxy <-
+  function(parm,
+           p_item,
+           prox_data,
+           responses_item,
+           pred_data,
+           cov,
+           samp_size,
+           num_items) {
 
+    sigma <- sqrt(p_item[grep("s0",names(p_item))][1]*exp(
+      pred_data %*% p_item[grep("s1",names(p_item))]))
+    mu <- (p_item[grep("c0",names(p_item),fixed=T)] +
+                      pred_data %*%
+                      p_item[grep("c1",names(p_item),fixed=T)]) +
+                     (p_item[grep("a0",names(p_item),fixed=T)] +
+                        pred_data %*%
+                        p_item[grep("a1",names(p_item),fixed=T)])*prox_data
+
+    if(parm == "s0") {
+      eta_d1 <- sapply(1:samp_size,
+                       function(x) {
+                         exp(pred_data[x,] %*%
+                               p_item[grep("s1",names(p_item))]) / (2*sigma[x])
+                       })
+      eta_d2 <- sapply(1:samp_size,
+                       function(x) {
+                         -exp(pred_data[x,] %*%
+                                p_item[grep("s1",names(p_item))])**2 /
+                           (4*sigma[x]**3)
+                       })
+    } else if(parm == "s1") {
+      eta_d1 <- sapply(1:samp_size,
+                       function(x) {
+                         sigma[x]*pred_data[x,cov] / 2
+                       })
+      eta_d2 <- sapply(1:samp_size,
+                       function(x) {
+                         sigma[x]*pred_data[x,cov]**2 / 4
+                       })
+    }
+
+
+    d1_trace <- t(sapply(1:samp_size,
+                         function(x) {
+                           eta_d1[x]*((responses_item[x]-mu[x,])**2 /
+                                        sigma[x]**3 -
+                                        1/sigma[x])
+                         }))
+
+    if(parm == "s0") {
+      d2_trace <- t(sapply(1:samp_size,
+                           function(x) {
+                             eta_d1[x]**2*(1 / sigma[x]**2 -
+                                             3*(responses_item[x] - mu[x,])**2 /
+                                             sigma[x]**4) +
+                               eta_d2[x]*((responses_item[x] - mu[x,])**2 /
+                                            sigma[x]**3 - 1/sigma[x])
+                           }))
+    } else if(parm == "s1") {
+      d2_trace <- t(sapply(1:samp_size,
+                           function(x) {
+                             -2*eta_d2[x]*(sigma[x]**(-3)*(responses_item[x] -
+                                                             mu[x,])**2)
+                           }))
+    }
+
+    d1 <- sum(d1_trace, na.rm = TRUE)
+    d2 <- sum(d2_trace, na.rm = TRUE)
+
+    dlist <- list(d1,d2)
+
+  }
 
